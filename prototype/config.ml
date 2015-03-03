85709458cdc5018d2452f6aa4f49e37b4a96519d
Mindy Preston <meetup@yomimono.org>
2015-03-03 10:56:36 +0000

    Insert tracing hooks.

--- prototype/config.ml
+++ prototype/config.ml
@@ -6,7 +6,9 @@ let main =
 let disk1 = crunch "pcaps"
 let tap = tap0
 
+let tracing = mprof_trace ~size:100000 ()
+
 let () =
   add_to_opam_packages["pcap-format"];
   add_to_ocamlfind_libraries["pcap-format"];
-  register "parrot" [ main $ default_console $ disk1 $ tap0 ]
+  register "parrot" ~tracing [ main $ default_console $ disk1 $ tap0 ]
