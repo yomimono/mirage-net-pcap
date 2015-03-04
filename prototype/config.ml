87dabb0faaab47b3fa9e65d6241d3a1fddebde1a
Mindy Preston <meetup@yomimono.org>
2015-03-04 18:18:46 +0000

    add dependencies for network stack

--- prototype/config.ml
+++ prototype/config.ml
@@ -1,7 +1,7 @@
 open Mirage
 
 let main =
-  foreign "Unikernel.Main" (console @-> kv_ro @-> network @-> job)
+  foreign "Unikernel.Main" (console @-> kv_ro @->  job)
 
 let disk1 = crunch "pcaps"
 let tap = tap0
@@ -9,6 +9,7 @@ let tap = tap0
 let tracing = mprof_trace ~size:100000 ()
 
 let () =
-  add_to_opam_packages["pcap-format"];
-  add_to_ocamlfind_libraries["pcap-format"];
-  register "parrot" ~tracing [ main $ default_console $ disk1 $ tap0 ]
+  add_to_opam_packages["pcap-format"; "tcpip"];
+  add_to_ocamlfind_libraries["pcap-format"; "tcpip.ethif"; "tcpip.ipv4";
+                             "tcpip.udp"; "tcpip.dhcpv4"];
+  register "parrot" ~tracing [ main $ default_console $ disk1 ]
