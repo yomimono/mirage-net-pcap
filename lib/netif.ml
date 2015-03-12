5a3e748cd0e3b0013570601e8b367d1b653dddac
Mindy Preston <meetup@yomimono.org>
2015-03-12 12:27:25 +0000

    license/copyright

--- lib/netif.ml
+++ lib/netif.ml
@@ -1,3 +1,20 @@
+(*                                                                              
+ * Copyright (c) 2015 Mindy Preston <meetup@yomimono.org>                  
+ *                                                                              
+ * Permission to use, copy, modify, and distribute this software for any        
+ * purpose with or without fee is hereby granted, provided that the above       
+ * copyright notice and this permission notice appear in all copies.            
+ *                                                                              
+ * THE SOFTWARE IS PROVIDED "AS IS" AND THE AUTHOR DISCLAIMS ALL WARRANTIES     
+ * WITH REGARD TO THIS SOFTWARE INCLUDING ALL IMPLIED WARRANTIES OF             
+ * MERCHANTABILITY AND FITNESS. IN NO EVENT SHALL THE AUTHOR BE LIABLE FOR      
+ * ANY SPECIAL, DIRECT, INDIRECT, OR CONSEQUENTIAL DAMAGES OR ANY
+ * DAMAGES       
+ * WHATSOEVER RESULTING FROM LOSS OF USE, DATA OR PROFITS, WHETHER IN AN        
+ * ACTION OF CONTRACT, NEGLIGENCE OR OTHER TORTIOUS ACTION, ARISING OUT OF      
+ * OR IN CONNECTION WITH THE USE OR PERFORMANCE OF THIS SOFTWARE.               
+ *)     
+
 module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
   type 'a io = 'a Lwt.t
   type page_aligned_buffer = Io_page.t
@@ -140,8 +157,8 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
             match t.source.timing with
             | None -> (set_last_read t (Some this_time), 0.0)
             | Some timing ->
-            (set_last_read t (Some this_time)), ((this_time -. last_time) *.
-                                                 timing)
+              (set_last_read t (Some this_time)), ((this_time -. last_time) *.
+                                                   timing)
         in
         read_wrapper t.source t.seek packet_size >>= function
         | None -> Lwt.return None
