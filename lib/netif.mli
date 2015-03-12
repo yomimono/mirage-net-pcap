e545a407457f96c4288304ee12aa4a10596386bc
Mindy Preston <meetup@yomimono.org>
2015-03-12 11:20:02 +0000

    slightly better explanations in comments

--- lib/netif.mli
+++ lib/netif.mli
@@ -5,13 +5,15 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) : sig
      and type buffer = Cstruct.t
      and type macaddr = Macaddr.t
 
+  val connect : id -> [ `Error of error | `Ok of t ] io
+
   (* use "timing" to accelerate or decelerate playback of packets.  1.0 is
      playback at the original recorded rate.  numbers greater than 1.0 will
      delay; numbers smaller than 1.0 will speed up playback.  None gives no
-     delay at all. *)
+     artificial delay and plays back packets as quickly as possible. *)
   val id_of_desc : ?timing:float option -> source:K.t -> read:string -> id
-  val connect : id -> [ `Error of error | `Ok of t ] io
 
-  (* return all packets written to this netif *)
+  (* return all frames written to this netif, in the order they were written.
+     Each element in the list represents the contents of a call to `write`. *)
   val get_written : t -> Cstruct.t list
 end
