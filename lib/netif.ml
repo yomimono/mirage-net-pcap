92efe77766b8f8249cecbb250a9652088b685a64
Mindy Preston <meetup@yomimono.org>
2015-03-12 11:21:39 +0000

    remove comment cruft; whitespace fixes

--- lib/netif.ml
+++ lib/netif.ml
@@ -3,8 +3,8 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
   type page_aligned_buffer = Io_page.t
   type buffer = Cstruct.t
   type macaddr = Macaddr.t
-  type error = [ `Unimplemented 
-               | `Disconnected 
+  type error = [ `Unimplemented
+               | `Disconnected
                | `Unknown of string ]
 
   type read_result = [
@@ -16,18 +16,18 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
     mutable rx_bytes : int64;
     mutable rx_pkts : int32;
     mutable tx_bytes : int64;
-    mutable tx_pkts : int32; 
+    mutable tx_pkts : int32;
   }
 
   type id = {
     timing : float option;
     source : K.t;
-    file : string; 
+    file : string;
   }
 
   type t = {
-    source : id; (* should really be something like an fd *) 
-    seek : int; 
+    source : id; (* should really be something like an fd *)
+    seek : int;
     last_read : float option;
     stats : stats;
     reader : (module Pcap.HDR);
@@ -40,7 +40,7 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
     rx_bytes = 0L;
     rx_pkts = 0l;
     tx_bytes = 0L;
-    tx_pkts = 0l; 
+    tx_pkts = 0l;
   }
 
   let string_of_t t =
@@ -52,14 +52,14 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
     | Some f -> { timing = f; source; file = read; }
     | None -> { timing = Some 1.0; source; file = read; }
 
-  let id t = t.source 
-  let connect (i : id) = 
+  let id t = t.source
+  let connect (i : id) =
     let open Lwt in
-    K.read i.source i.file 0 (Pcap.sizeof_pcap_header) >>= 
+    K.read i.source i.file 0 (Pcap.sizeof_pcap_header) >>=
     fun result ->
-    match result with 
-    | `Error _ -> Lwt.return (`Error (`Unknown "file could not be read") ) 
-    | `Ok bufs ->  (* huh, apparently this can return us an empty list? *)
+    match result with
+    | `Error _ -> Lwt.return (`Error (`Unknown "file could not be read") )
+    | `Ok bufs ->
       match bufs with
       | [] -> Lwt.return (`Error (`Unknown "empty file"))
       | hd :: _ ->
@@ -67,7 +67,7 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
         match Pcap.detect hd with
         | None -> Lwt.return (`Error (`Unknown "file could not be parsed"))
         | Some reader ->
-          Lwt.return (`Ok { 
+          Lwt.return (`Ok {
               source = i;
               seek = Pcap.sizeof_pcap_header;
               last_read = None;
@@ -76,7 +76,7 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
               written = ref [];
             })
 
-  let disconnect t = 
+  let disconnect t =
     Lwt.return_unit
 
   let writev t bufs = t.written := t.!written @ bufs; Lwt.return_unit
@@ -86,14 +86,14 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
 
   let advance_seek t seek = { t with seek = (t.seek + seek); }
 
-  let set_last_read t last_read = 
+  let set_last_read t last_read =
     { t with last_read = last_read; }
 
   (* merge bufs into one big cstruct. *)
-  let combine_cstructs l = 
+  let combine_cstructs l =
     match l with
     | hd :: [] -> hd
-    | _ -> 
+    | _ ->
       let consolidated = Cstruct.create (Cstruct.lenv l) in
       let fill seek buf =
         Cstruct.blit buf 0 consolidated seek (Cstruct.len buf);
@@ -102,20 +102,18 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
       ignore (List.fold_left fill 0 l);
       consolidated
 
-  let rec listen t cb = 
-    let open Lwt in 
+  let rec listen t cb =
+    let open Lwt in
     let read_wrapper (i : id) seek how_many =
       K.read i.source i.file seek how_many >>= function
-      | `Ok [] -> Lwt.return None (* raise (Invalid_argument 
-                           (Printf.sprintf "read an empty list, requested %d
-                           from %d" how_many seek)) *)
+      | `Ok [] -> Lwt.return None
       | `Ok (buf :: []) -> Lwt.return (Some (Cstruct.sub buf 0 how_many))
       | `Ok bufs -> Lwt.return (Some (combine_cstructs bufs))
       | `Error _ -> raise (Invalid_argument "Read failed")
     in
     let next_packet t =
-      read_wrapper t.source t.seek Pcap.sizeof_pcap_packet >>= 
-      function 
+      read_wrapper t.source t.seek Pcap.sizeof_pcap_packet >>=
+      function
       | None -> Lwt.return None
       | Some packet_header ->
         let t = advance_seek t Pcap.sizeof_pcap_packet in
@@ -125,23 +123,23 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
                                           packet_header) in
         let packet_secs = R.get_pcap_packet_ts_sec packet_header in
         let packet_usecs = R.get_pcap_packet_ts_usec packet_header in
-        let (t, delay) = 
-          let pack (secs, usecs) = 
+        let (t, delay) =
+          let pack (secs, usecs) =
             let secs_of_usecs = (1.0 /. 1000000.0) in
-            (float_of_int (Int32.to_int secs)) +. 
+            (float_of_int (Int32.to_int secs)) +.
             ((float_of_int (Int32.to_int usecs)) *. secs_of_usecs)
           in
           let this_time = pack (packet_secs, packet_usecs) in
           match t.last_read with
-          | None -> (set_last_read t (Some this_time), 0.0) 
+          | None -> (set_last_read t (Some this_time), 0.0)
           | Some last_time ->
             match t.source.timing with
             | None -> (set_last_read t (Some this_time), 0.0)
             | Some timing ->
             (set_last_read t (Some this_time)), ((this_time -. last_time) *.
-                                                 timing) 
+                                                 timing)
         in
-        read_wrapper t.source t.seek packet_size >>= function 
+        read_wrapper t.source t.seek packet_size >>= function
         | None -> Lwt.return None
         | Some packet_body ->
           let t = advance_seek t (packet_size) in
@@ -149,9 +147,9 @@ module Make (K: V1_LWT.KV_RO) (T: V1_LWT.TIME) = struct
     in
     next_packet t >>= function
     | None -> Lwt.return_unit
-    | Some (next_t, delay, packet) -> 
-      T.sleep delay >>= fun () -> 
-      cb packet >>= fun () -> 
+    | Some (next_t, delay, packet) ->
+      T.sleep delay >>= fun () ->
+      cb packet >>= fun () ->
       listen next_t cb
 
   let mac t = Macaddr.broadcast (* arbitrarily *)
