fe6a63d7d5ef9ca42a6b10a257277a2b759274fb
Mindy Preston <meetup@yomimono.org>
2015-05-13 23:00:50 +0100

    pcap_write.ml: TODO done

--- lib/pcap_write.ml
+++ lib/pcap_write.ml
@@ -58,9 +58,6 @@ module Make (Pcap_impl : Pcap.HDR) (FS : V1_LWT.FS with type page_aligned_buffer
     let header_size = Pcap.sizeof_pcap_packet in
     let packet_size = Cstruct.len packet in
     let copy_size = min 65535 packet_size in
-    (* TODO: shouldn't claim we wrote > 65535 bytes; if we do that, the caller
-       will advance the seek pointer further than we actually wrote *)
-    (* also, shouldn't attempt to write >65535 bytes of data! *)
     let header = Cstruct.create (header_size + copy_size) in
     create_packet_header header time packet_size;
     Cstruct.blit packet 0 header header_size copy_size;
